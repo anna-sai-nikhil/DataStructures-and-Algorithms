7f71c38cd71f84b6fb3a768d1864f5b4d7d1f2ac
anna-sai-nikhil <annasainikhil0@gmail.com>
2023-03-12 00:39:39 +0530

    stack

---
+++ Stack/Main.java
@@ -0,0 +1,10 @@
+package Stack;
+
+public class Main {
+    public static void main(String[] args){
+        Stack stack = new Stack();
+        for(int i=1;i<11;i++) stack.push(i);
+
+    }
+    
+}
---
+++ Stack/Stack.java
@@ -0,0 +1,29 @@
+package Stack;
+
+public class Stack {
+    int[] array = new int[5];
+    int count = 0;
+
+    public void push(int item) {
+        if (count < array.length)
+            array[count++] = item;
+        else {
+            int[] arr = new int[array.length * 2];
+            for (int i = 0; i < count; i++)
+                arr[i] = array[i];
+            array = arr;
+            array[count++] = item;
+
+        }
+    }
+    public void peek(){
+        if(count==0) throw new RuntimeException("Deklo bai");
+        System.out.println(array[count-1]);
+    }
+    public int pop(){
+        if(count==0) throw new RuntimeException("Deklo bai");
+        return array[--count];
+    }
+
+
+}
